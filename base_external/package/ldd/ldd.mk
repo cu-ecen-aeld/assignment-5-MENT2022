08e3ad2dd7b87fe7cec1a6cabc59bd39c35be836
Mouin EDDEB <mouin.eddeb@gmail.com>
2025-05-02 20:08:31 +0200

    Added ldd package for building misc-modules and scull

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,13 +4,22 @@
 #
 ################################################################################
 
-LDD_VERSION = '1.0'
-LDD_SITE = 'https://github.com/cu-ecen-aeld/assignment-7-MENT2022.git'
+LDD_VERSION = 'main'
+LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-MENT2022.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules scull
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+define LDD_BUILD_CMDS
+    $(MAKE) -C $(@D)/misc-modules $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
+    $(MAKE) -C $(@D)/scull $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/misc-modules/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/
+    $(INSTALL) -D -m 0755 $(@D)/scull/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
